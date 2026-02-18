040eed988e0f3b46a7be04f44f0bd3f45f264389
Malcolm Benedict <mmbenedi@mtu.edu>
2026-02-18 16:04:53 -0500

    First draft of UKF

--- src/kalman_filter/kalman_filter/kalman.py
+++ src/kalman_filter/kalman_filter/kalman.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import float64
 
 class KalmanFilter:
     def __init__(self, F, H, Q, R, B, x_0, P_0) -> None:
@@ -46,6 +47,71 @@ class KalmanFilter:
         print("\n")
         return x, residual
 
+class UnscentedKalmanFilter:
+    """
+    Docstring for UnscentedKalmanFilter
+    Loop structure based on https://github.com/balghane/pyUKF/blob/master/ukf.py
+    """
+    def __init__(self, F, H, Q, R, x_0, P_0, deltaT) -> None:
+        self.F = F
+        self.H = H
+        self.Q = Q
+        self.R = R
+        self.x = x_0
+        self.Pxx = P_0
+        self.deltaT = deltaT
+        self.alpha = 0.001
+        self.k = 0 
+        self.beta = 2
+        self.n = len(self.x[0])
+        self.n_sigma = (2 * self.n) + 1
+        self.lambda_= + pow(self.alpha, 2) * (self.n + self.k) - self.n
+        self.covariance_weights,self.mean_weights = self.get_weights()
+
+    def get_weights(self):
+        self.covariance_weights[0] = (self.lambda_ / (self.n + self.lambda_)) + (1 - pow(self.alpha, 2) + self.beta)
+        self.mean_weights[0] = (self.lambda_ / (self.n + self.lambda_))
+        for i in range(1,(2*self.n)):
+            self.covariance_weights[i] = 1/(2*(self.n - self.lambda_))
+            self.mean_weights[i] = 1/(2*(self.n - self.lambda_))
+        return self.covariance_weights, self.mean_weights
+
+    def get_sigma_points(self):
+        self.L = np.linalg.cholesky(np.dot((self.n + self.lambda_),self.Pxx))
+        self.xbar = np.average(self.x)
+        sigmaT = []
+        sigmaT[0] = self.xbar
+        for i in range(1,self.n):
+            sigmaT[i] = self.xbar + self.L
+            sigmaT[i+self.n] = self.xbar - self.L
+        self.sigma = np.transpose(sigmaT)
+        return self.sigma
+
+    def predict(self):
+        self.sigma = self.get_sigma_points()
+        self.y = np.dot(self.F,self.sigma)
+        for i in range(2*self.n):
+            self.ybar = self.ybar + np.dot(self.mean_weights[i], self.y[i])
+        for i in range(2*self.n):
+            self.Pyy = self.Pyy + np.dot(self.covariance_weights[i],np.dot((self.y[i] - self.ybar),np.transpose((self.y[i] - self.ybar)))) + self.Q
+        return self.y
+
+    def update(self,z,H):
+        self.z = z
+        self.H = H
+        self.z = np.dot(self.H,self.y)
+        for i in range(2*self.n):
+            self.zbar = self.zbar + np.dot(self.mean_weights[i], self.z[i])
+        for i in range(2*self.n):
+            self.Pzz = self.Pzz + np.dot(self.covariance_weights[i],np.dot((self.z[i] - self.zbar),np.transpose((self.z[i] - self.zbar)))) + self.R
+        for i in range(2*self.n):
+            self.Pyz = self.Pyz + np.dot(self.covariance_weights[i],np.dot((self.y[i] - self.ybar),np.transpose((self.z[i] - self.zbar))))
+        self.K = np.dot(self.Pyz,np.linalg.inv(self.Pzz))
+        self.residual = self.z - self.zbar
+        self.x = self.ybar - np.dot(self.K, self.residual)
+        self.P = self.Pyy - np.dot(self.K,np.dot(self.Pzz,np.transpose(self.K)))
+        return self.x, self.P
+    
 class ExtendedKalmanFilter:
     def __init__(self, F, H, Q, R, B, x_0, P_0) -> None:
         self.F = F
--- src/kalman_filter/kalman_filter/turtlebot_filter.py
+++ src/kalman_filter/kalman_filter/turtlebot_filter.py
@@ -6,8 +6,8 @@ from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy
 from sensor_msgs.msg import JointState, Imu
 from geometry_msgs.msg import TwistStamped, PoseStamped
 from nav_msgs.msg import Path, Odometry
-from kalman_filter.kalman import KalmanFilter, ExtendedKalmanFilter
-from kalman_filter.kalman_prams import Kf_prams,Ekf_prams
+from kalman_filter.kalman import KalmanFilter, ExtendedKalmanFilter, UnscentedKalmanFilter
+from kalman_filter.kalman_prams import Kf_prams,Ekf_prams, Ukf_prams
 from rclpy.clock import Clock
 from kalman_filter.euler_quaternion import convertEulerToQuaternion
 
@@ -23,9 +23,10 @@ class Localizer(Node):
 
         self.kf = Kf_prams(self.delta_t)
         self.ekf = Ekf_prams(self.delta_t)
+        self.ukf = Ukf_prams(self.delta_t)
         self.kalman_filter = KalmanFilter(self.kf.F, self.kf.H, self.kf.Q, self.kf.R, self.kf.B, self.kf.x0, self.kf.P0)
         self.extended_kalman_filter = ExtendedKalmanFilter(self.ekf.F, self.ekf.H, self.ekf.Q, self.ekf.R, self.ekf.B, self.ekf.x0, self.ekf.P0)
-
+        self.unscented_kalman_filter = UnscentedKalmanFilter(self.ukf.F, self.ukf.H, self.ukf.Q, self.ukf.R, self.ukf.x0, self.ukf.P0,self.delta_t)
 
         subscriber_qos_profile = QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT)
         self.joint_state_subscription = self.create_subscription(JointState,'/joint_states', self.joint_state_callback, subscriber_qos_profile)
@@ -41,11 +42,12 @@ class Localizer(Node):
         self.kf_path_arr = []
 
         self.ekf_path_publisher = self.create_publisher(Path, 'localization_node/ekf/path', publisher_qos_profile)
-        self.ukf_path_publisher = self.create_publisher(Path, 'localization_node/ukf/path', publisher_qos_profile)
+        self.ekf_odom_publisher = self.create_publisher(Odometry, 'localization_node/ekf/odometry', publisher_qos_profile)
         self.ekf_path_arr = []
 
-        self.ekf_odom_publisher = self.create_publisher(Odometry, 'localization_node/ekf/odometry', publisher_qos_profile)
+        self.ukf_path_publisher = self.create_publisher(Path, 'localization_node/ukf/path', publisher_qos_profile)
         self.ukf_odom_publisher = self.create_publisher(Odometry, 'localization_node/ukf/odometry', publisher_qos_profile)
+        self.ukf_path_arr = []
 
         self.js_right_wheel = 0
         self.js_left_wheel = 0
@@ -140,6 +142,40 @@ class Localizer(Node):
                       [world_w],
                       [0]])
         return self.extended_kalman_filter.update(z,H)
+    
+    def ukf_predict(self):
+        theta = self.unscented_kalman_filter.x[6,0]
+        world_x_vel = (self.cmd_vel_x*m.cos(theta)) - (self.cmd_vel_y*m.sin(theta))
+        world_y_vel = (self.cmd_vel_x*m.sin(theta)) + (self.cmd_vel_y*m.cos(theta))
+        world_w = self.cmd_vel_w
+        u = np.array([[0],
+                      [world_x_vel],
+                      [0],
+                      [0],
+                      [world_y_vel],
+                      [0],
+                      [0],
+                      [world_w],
+                      [0]])
+        return self.unscented_kalman_filter.predict()
+
+    def ukf_update(self):
+        theta = self.unscented_kalman_filter.x[6,0]
+        x_acc = self.unscented_kalman_filter.x[2,0]
+        y_acc = self.unscented_kalman_filter.x[5,0]
+        H = self.ukf.update_prams(theta,x_acc,y_acc)
+        world_t = ((ROBOT_WHEEL_RADIUS*(self.js_right_wheel-self.js_left_wheel))/(ROBOT_WHEELBASE))-(m.pi/2)
+        world_w = self.cmd_vel_w
+        z = np.array([[0],
+                      [0],
+                      [self.imu_ax],
+                      [0],
+                      [0],
+                      [self.imu_ay], 
+                      [world_t],
+                      [world_w],
+                      [0]])
+        return self.unscented_kalman_filter.update(z,H)
 
     def timer_callback(self):
         if(self.FRESH_JOINT_STATE and self.FRESH_IMU):
@@ -148,16 +184,21 @@ class Localizer(Node):
             kf_x_update, kf_residual = self.kf_update()
             ekf_x_prediction = self.ekf_predict()
             ekf_x_update, ekf_residual = self.ekf_update()
+            ukf_x_prediction = self.ukf_predict()
+            ukf_x_update, ukf_P = self.ukf_update()
             #print("updating!")
             self.FRESH_JOINT_STATE = False
             self.FRESH_IMU = False
             self.FRESH_CMD_VEL = False
             kf_odom, kf_path, self.kf_path_arr = self.message_generator(kf_x_update[0,0],kf_x_update[3,0],kf_x_update[6,0],self.kf_path_arr)
             ekf_odom, ekf_path, self.ekf_path_arr = self.message_generator(ekf_x_update[0,0],ekf_x_update[3,0],ekf_x_update[6,0],self.ekf_path_arr)
+            ukf_odom, ukf_path, self.ukf_path_arr = self.message_generator(ukf_x_update[0,0],ukf_x_update[3,0],ukf_x_update[6,0],self.ukf_path_arr)
             self.kf_path_publisher.publish(kf_path)
             self.kf_odom_publisher.publish(kf_odom)
             self.ekf_path_publisher.publish(ekf_path)
             self.ekf_odom_publisher.publish(ekf_odom)
+            self.ukf_path_publisher.publish(ukf_path)
+            self.ukf_odom_publisher.publish(ukf_odom)
 
     def message_generator(self,x,y,t,path_arr):
             #generate pose message
